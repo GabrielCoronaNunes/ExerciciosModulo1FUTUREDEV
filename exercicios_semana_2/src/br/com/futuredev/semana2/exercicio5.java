3f9c3378234cc147ac2cb5888a8fe872b3b4858a
GabrielCoronaNunes <110194014+GabrielCoronaNunes@users.noreply.github.com>
2022-07-28 16:34:20 -0300

    Update exercicio5.java

--- exercicios_semana_2/src/br/com/futuredev/semana2/exercicio5.java
+++ exercicios_semana_2/src/br/com/futuredev/semana2/exercicio5.java
@@ -1,3 +1,7 @@
+//[M1S02] Ex 5 - Média
+//Faça um programa que leia as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média. Se a nota for maior ou igual a 6.0, deve ser apresentada a mensagem “Aprovado”, caso contrário deve ser apresentada a mensagem “Reprovado”.
+
+
 package br.com.futuredev.semana2;
 
 import java.util.Scanner;

b899f3f5bac95f95602b51f21b0e327058d121f9
GabrielCoronaNunes <110194014+GabrielCoronaNunes@users.noreply.github.com>
2022-07-28 16:34:43 -0300

    Update exercicio6.java

--- exercicios_semana_2/src/br/com/futuredev/semana2/exercicio6.java
+++ exercicios_semana_2/src/br/com/futuredev/semana2/exercicio6.java
@@ -1,3 +1,7 @@
+//[M1S02] Ex 6 - Triângulos
+//Faça um programa que peça os 3 lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
+
+
 package br.com.futuredev.semana2;
 
 import java.util.Scanner;
